31b572c95e2b7d0167d1b2bde7011e2bc1920059
AranL152 <184890929+AranL152@users.noreply.github.com>
2026-01-31 23:29:48 -0500

    5

--- Two Sum.py
+++ Two Sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ret = []
-        check = {}
+        me = {}
 
         for i, num in enumerate(nums):
-            if (target-num) in check:
+            if (target-num) in me:
                 ret.append(i)
-                ret.append(check[target-num])
-            check[num] = i
+                ret.append(me[target-num])
+            me[num] = i
         
         return ret
